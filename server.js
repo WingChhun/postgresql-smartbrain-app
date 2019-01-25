cd6eb86e38e5f29b08dccf445a69b74355c20afc
James Chhun <capt.wing.chhun@gmail.com>
2019-01-25 09:36:34 -0800

    Create image, signin, register route all using knex query builder: Have matching postgresql statements written

--- server.js
+++ server.js
@@ -43,19 +43,106 @@ app.get('/', (req, res) => {
 
 app.get('/select', (req, res) => {});
 
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+
+  const error = `User with id ${id} not found`;
+
+  /*
+  Note: returns an array, returning an empty array returns a 200-status code: so check if there is a user.length: return 200 / 400 accordingly
+  */
+  db.select('*')
+    .from('users')
+    .where('id', id)
+    //$ Success getting a user array:
+    .then(user =>
+      user.length ? res.status(200).json(user[0]) : res.status(400).json(error)
+    )
+    //! Error gettinga  user
+    .catch(err => res.status(400).json('User not found'));
+});
+
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
 
+  const hash = bcrypt.hashSync(password);
   const error = `Unable to register ${email}`;
 
-  //TODO: dont have passwor yet
+  //TODO: update users and login
+  // ? - Transaction: if one fails they all fail
+  db.transaction(trx => {
+    //TODO: dont have passwor yet
+
+    trx
+      .insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return (
+          trx('users')
+            .returning('*')
+            .insert({ email: loginEmail[0], name, joined: new Date() })
+            //$ Success
+            .then(response => res.status(200).json(response[0]))
+            //! Error : Dont return database error
+            .catch(err => res.status(400).json(error))
+        );
+      })
+
+      //$ All pass: commit the change
+      .then(trx.commit)
+      //! Fail roll back db
+      .catch(trx.rollback);
+  });
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+
   db('users')
-    .returning('*')
-    .insert({ email, name, joined: new Date() })
-    //$ Success
-    .then(response => res.status(200).json(response[0]))
-    //! Error : Dont return database error
-    .catch(err => res.status(400).json(error));
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => res.status(200).json(entries))
+    .catch(err => res.status(400).json(err));
+});
+
+app.post('/signin', (req, res) => {
+  //SQL - SELECT email, hash FROM login WHERE email=req.boy.email; `
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      //Note: Pull password
+      const { hash } = data[0];
+
+      const isValid = bcrypt.compareSync(req.body.password, hash);
+
+      //$ Success query for the user in db w/ matchibg email
+
+      //SQL `SELECT * FROM users WHERE email=req.body.email`
+      if (isValid) {
+        db.select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            //note: We also get an array so return user[0]
+            return res.status(200).json(user[0]);
+          })
+          .catch(err =>
+            res.status(400).json('No user registered with this email')
+          );
+      } else {
+        return res.status(400).json('Wrong credentials');
+      }
+    })
+    //! Error handler
+    .catch(err => {
+      return res.status(400).json('Invalid login credentials');
+    });
 });
 
 //Note: start server
